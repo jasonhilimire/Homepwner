b137a5df1b869155484005d3b6acb9d449b31223
jasonhilimire <jasonhilimire@mac.com>
2018-10-21 19:49:13 -0600

    initial commit

---
+++ Homepwner/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Jason Hilimire on 10/21/18.
+//  Copyright © 2018 Peanut Apps. All rights reserved.
+//
+
+import UIKit
+
+
+class Item: NSObject {
+    
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+}
---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Jason Hilimire on 10/21/18.
+//  Copyright © 2018 Peanut Apps. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+    
+    
+}
