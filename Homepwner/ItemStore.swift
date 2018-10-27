163c5bc63db8da63c644eaf9f47d9f5b2cdf69f8
jasonhilimire <jasonhilimire@mac.com>
2018-10-27 15:27:59 -0600

    added buttons properly

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -12,13 +12,7 @@ class ItemStore {
     
     var allItems = [Item]()
     
-    @IBAction func addNewItem(_ sender: UIButton) {
-        
-    }
-    
-    @IBAction func toggleEditingMode(_ sender: UIButton) {
-        
-    }
+
     
     @discardableResult func createItem() -> Item {
     let newItem = Item(random: true)
