fa897f7bb6657d9acf8570f0e3b1ee3fc081ad8a
jasonhilimire <jasonhilimire@mac.com>
2018-11-05 19:37:19 -0700

    Setup ItemCell

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Jason Hilimire on 11/5/18.
+//  Copyright © 2018 Peanut Apps. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    
+}
